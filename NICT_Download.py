cae471b34116456051bcb3c5c57f0f0ea347056e
FredLin0513 <69899400+FredLin0513@users.noreply.github.com>
2024-06-23 11:38:31 +0800

    Update NICT_Download.py

--- NICT_Download.py
+++ NICT_Download.py
@@ -15,23 +15,64 @@ def download_img(url, img_save_path):
 		print(img_save_path + "图片下载成功")
 
 
-def fill_img(img_0,img_1,img_2,img_3,dir,img_save_path):
-	width, height = 1920, 1080      # 电脑屏幕大小
-	#new_img = Image.new(img_1.mode, (width, height), color='black')
+def fill_img(img_0,img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8,img_9,img_10,img_11,img_12,img_13,img_14,img_15,dir,img_save_path):
+	#图片位置示意
+	#0 4 8  12   
+	#1 5 9  13
+	#2 6 10 14
+	#3 7 11 15
+	width, height = 2560, 1600      # 电脑屏幕大小
+	new_img = Image.new(img_1.mode, (width, height), color='black')
 	new_img = Image.open(dir+"images/background.png")
 	new_img = new_img.convert("RGBA")
-	img_0=img_0.crop((0,10,550,550))
-	img_1=img_1.crop((0,0 ,550,540))
-	img_2=img_2.crop((0,10,550,550))
-	img_3=img_3.crop((0,0 ,550,540))
+	img_0=img_0.crop((0,0,550,550))
+	img_1=img_1.crop((0,0,550,550))
+	img_2=img_2.crop((0,0,550,550))
+	img_3=img_3.crop((0,0,550,550))
+	img_4=img_4.crop((0,0,550,550))
+	img_5=img_5.crop((0,0,550,550))
+	img_6=img_6.crop((0,0,550,550))
+	img_7=img_7.crop((0,0,550,550))
+	img_8=img_8.crop((0,0,550,550))
+	img_9=img_9.crop((0,0,550,550))
+	img_10=img_10.crop((0,0,550,550))
+	img_11=img_11.crop((0,0,550,550))
+	img_12=img_12.crop((0,0,550,550))
+	img_13=img_13.crop((0,0,550,550))
+	img_14=img_14.crop((0,0,550,550))
+	img_15=img_15.crop((0,0,550,550))
 	r,g,b,a = img_0.split()
-	new_img.paste(img_0, (410,  0),mask = a)
+	new_img.paste(img_0, (331,0),mask = a)#图片左上角的xy坐标
 	r,g,b,a = img_1.split()
-	new_img.paste(img_1, (410,540),mask = a)
+	new_img.paste(img_1, (331,550),mask = a)
 	r,g,b,a = img_2.split()
-	new_img.paste(img_2, (960,  0),mask = a)
+	new_img.paste(img_2, (331,1100),mask = a)
 	r,g,b,a = img_3.split()
-	new_img.paste(img_3, (960,540),mask = a)
+	new_img.paste(img_3, (331,1650),mask = a)
+	r,g,b,a = img_4.split()
+	new_img.paste(img_4, (881,0),mask = a)
+	r,g,b,a = img_5.split()
+	new_img.paste(img_5, (881,550),mask = a)
+	r,g,b,a = img_6.split()
+	new_img.paste(img_6, (881,1100),mask = a)
+	r,g,b,a = img_7.split()
+	new_img.paste(img_7, (881,1650),mask = a)
+	r,g,b,a = img_8.split()
+	new_img.paste(img_8, (1431,0),mask = a)
+	r,g,b,a = img_9.split()
+	new_img.paste(img_9, (1431,550),mask = a)
+	r,g,b,a = img_10.split()
+	new_img.paste(img_10, (1431,1100),mask = a)
+	r,g,b,a = img_11.split()
+	new_img.paste(img_11, (1431,1650),mask = a)
+	r,g,b,a = img_12.split()
+	new_img.paste(img_12, (1981,0),mask = a)
+	r,g,b,a = img_13.split()
+	new_img.paste(img_13, (1981,550),mask = a)
+	r,g,b,a = img_14.split()
+	new_img.paste(img_14, (1981,1100),mask = a)
+	r,g,b,a = img_15.split()
+	new_img.paste(img_15, (1981,1650),mask = a)
 	new_img.save(img_save_path)
 	print(img_save_path + "图片合成成功")
 
@@ -39,7 +80,7 @@ def fill_img(img_0,img_1,img_2,img_3,dir,img_save_path):
 def dl_main(dir):
 	print("开始下载图片")
 	# 获取当前系统时间
-	utc_today = datetime.datetime.utcnow() - datetime.timedelta(minutes=30)  # 获取GMT时间并减去20分钟
+	utc_today = datetime.datetime.now(datetime.UTC) - datetime.timedelta(minutes=30)  # 获取GMT时间并减去20分钟
 	delat_utc_today = utc_today.strftime("%Y/%m/%d/%H%M")  # 时间格式化
 	# 分钟向下取整
 	delat_utc_today_list = list(delat_utc_today)
@@ -50,9 +91,9 @@ def dl_main(dir):
 	#                   https://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/2020/09/28/062000_0_0.png
 	
 	i=0
-	for row in range(2):
-		for col in range(2):
-			img_url = "https://himawari8-dl.nict.go.jp/himawari8/img/D531106/2d/550/%s00_%d_%d.png"%(delat_utc_today,row,col)
+	for row in range(4):
+		for col in range(4):
+			img_url = "https://himawari8-dl.nict.go.jp/himawari8/img/D531106/4d/550/%s00_%d_%d.png"%(delat_utc_today,row,col)
 			print(img_url)
 			name = delat_utc_today.replace("/", "_") + "00_%d_%d.png"%(row,col)  # 获取图片名字
 			# 图片保存路径
@@ -82,13 +123,36 @@ def dl_main(dir):
 				img_2 = img
 			elif i==3:
 				img_3 = img
-			
+			elif i==4:
+				img_4 = img
+			elif i==5:
+				img_5 = img
+			elif i==6:
+				img_6 = img
+			elif i==7:
+				img_7 = img
+			elif i==8:
+				img_8 = img
+			elif i==9:
+				img_9 = img
+			elif i==10:
+				img_10 = img
+			elif i==11:
+				img_11 = img
+			elif i==12:
+				img_12 = img
+			elif i==13:
+				img_13 = img
+			elif i==14:
+				img_14 = img
+			elif i==15:
+				img_15 = img
 			i=i+1
 	
 
 	new_img_save_path = dir+"Wallpaper/" + delat_utc_today.replace("/", "_")+".png"
 	#print(new_img_save_path)
-	fill_img(img_0,img_1,img_2,img_3,dir,new_img_save_path)
+	fill_img(img_0,img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8,img_9,img_10,img_11,img_12,img_13,img_14,img_15,dir,new_img_save_path)
 	return new_img_save_path
 
 
